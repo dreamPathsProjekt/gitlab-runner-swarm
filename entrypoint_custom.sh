700dfa7144ac3e27e80414bb276b4990b65c94ba
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2019-09-07 23:55:25 +0300

    Refactor register command as string & eval

--- entrypoint_custom.sh
+++ entrypoint_custom.sh
@@ -8,28 +8,16 @@ if [ -z "${GITLAB_URL}" ]; then
     GITLAB_URL=$(cat /run/secrets/gitlab_url)
 fi
 
-DOCKER_VOLUMES_COMMAND=''
+REGISTER_COMMAND="gitlab-runner register --non-interactive --executor ${RUNNER_EXECUTOR:-docker} --docker-image ${DOCKER_DEFAULT_IMAGE:-docker:latest} --url ${GITLAB_URL:-https://gitlab.com/} --registration-token ${PROJECT_REGISTRATION_TOKEN} --description ${RUNNER_DESCRIPTION:-docker-runner} --tag-list ${TAG_LIST:-docker} --run-untagged=${RUN_UNTAGGED:-true} --locked=${LOCKED:-false} --docker-privileged=${DOCKER_PRIVILEGED:-true} --access-level=${ACCESS_LEVEL:-not_protected}"
 
 if [ ! -z "${DOCKER_VOLUMES_LIST}" ]; then
     IFS=',' read -ra volumes <<< "${DOCKER_VOLUMES_LIST}"
     for volume in "${volumes[@]}"
     do
-        DOCKER_VOLUMES_COMMAND="${DOCKER_VOLUMES_COMMAND} --docker-volumes ${volume}"
+        REGISTER_COMMAND="${REGISTER_COMMAND} --docker-volumes ${volume}"
     done
 fi
 
-gitlab-runner register \
---non-interactive \
---executor "${RUNNER_EXECUTOR:-docker}" \
---docker-image "${DOCKER_DEFAULT_IMAGE:-docker:latest}" \
---url "${GITLAB_URL:-https://gitlab.com/}" \
---registration-token "${PROJECT_REGISTRATION_TOKEN}" \
---description "${RUNNER_DESCRIPTION:-docker-runner}" \
---tag-list "${TAG_LIST:-docker}" \
---run-untagged="${RUN_UNTAGGED:-true}" \
---locked="${LOCKED:-false}" \
---docker-privileged="${DOCKER_PRIVILEGED:-true}" \
---access-level="${ACCESS_LEVEL:-not_protected}" \
-"${DOCKER_VOLUMES_COMMAND}"
+eval $REGISTER_COMMAND
 
 exec /entrypoint "$@"

ddfba87367d758e2388e4a429b9ed0c8b6256da4
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2019-09-07 12:48:29 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM gitlab/gitlab-runner:latest
+LABEL maintainer="Ioannis Dritsas <dream.paths.projekt@gmail.com>"
+
+COPY entrypoint_custom.sh /
+RUN chmod a+x /entrypoint_custom.sh
+ENTRYPOINT ["/usr/bin/dumb-init", "/entrypoint_custom.sh"]
---
+++ entrypoint_custom.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "${PROJECT_REGISTRATION_TOKEN}" ]; then
+    PROJECT_REGISTRATION_TOKEN=$(cat /run/secrets/project_registration_token)
+fi
+
+if [ -z "${GITLAB_URL}" ]; then
+    GITLAB_URL=$(cat /run/secrets/gitlab_url)
+fi
+
+gitlab-runner register \
+        --non-interactive \
+        --executor "docker" \
+        --docker-image docker:latest \
+        --docker-volumes "${DOCKER_VOLUMES:-/var/run/docker.sock:/var/run/docker.sock}" \
+        --url "${GITLAB_URL:-https://gitlab.com/}" \
+        --registration-token "${PROJECT_REGISTRATION_TOKEN}" \
+        --description "${RUNNER_DESCRIPTION:-docker-runner}" \
+        --tag-list "${TAG_LIST:-docker}" \
+        --run-untagged="${RUN_UNTAGGED:-true}" \
+        --locked="${LOCKED:-false}" \
+        --docker-privileged="${DOCKER_PRIVILEGED:-true}" \
+        --access-level="${ACCESS_LEVEL:-not_protected}"
+
+exec "$@"

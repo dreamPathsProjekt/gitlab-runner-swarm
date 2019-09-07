4dbb06321ea3b140ed69501f2506eedadb8cfc64
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2019-09-07 22:39:52 +0300

    Switch to one line command to avoid splitting

--- entrypoint_custom.sh
+++ entrypoint_custom.sh
@@ -18,17 +18,6 @@ if [ ! -z "${DOCKER_VOLUMES_LIST}" ]; then
     done
 fi
 
-gitlab-runner register \
-        --non-interactive \
-        --executor "${RUNNER_EXECUTOR:-docker}" \
-        --docker-image "${DOCKER_DEFAULT_IMAGE:-docker:latest}" "${DOCKER_VOLUMES_COMMAND}" \
-        --url "${GITLAB_URL:-https://gitlab.com/}" \
-        --registration-token "${PROJECT_REGISTRATION_TOKEN}" \
-        --description "${RUNNER_DESCRIPTION:-docker-runner}" \
-        --tag-list "${TAG_LIST:-docker}" \
-        --run-untagged="${RUN_UNTAGGED:-true}" \
-        --locked="${LOCKED:-false}" \
-        --docker-privileged="${DOCKER_PRIVILEGED:-true}" \
-        --access-level="${ACCESS_LEVEL:-not_protected}"
+gitlab-runner register --non-interactive --executor "${RUNNER_EXECUTOR:-docker}" --docker-image "${DOCKER_DEFAULT_IMAGE:-docker:latest}" "${DOCKER_VOLUMES_COMMAND}" --url "${GITLAB_URL:-https://gitlab.com/}" --registration-token "${PROJECT_REGISTRATION_TOKEN}" --description "${RUNNER_DESCRIPTION:-docker-runner}" --tag-list "${TAG_LIST:-docker}" --run-untagged="${RUN_UNTAGGED:-true}" --locked="${LOCKED:-false}" --docker-privileged="${DOCKER_PRIVILEGED:-true}" --access-level="${ACCESS_LEVEL:-not_protected}"
 
 exec /entrypoint "$@"

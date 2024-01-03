2c6e6489d661f61e67031a4bd55baa75984314fd
Paul Saunders <p.saunders@cgi.com>
2024-01-03 13:58:07 +0000

    Add extra verbosity to entrypoint

--- entrypoint_custom.sh
+++ entrypoint_custom.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit  # abort on nonsero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
 
 if [ -z "${PROJECT_REGISTRATION_TOKEN}" ]; then
     PROJECT_REGISTRATION_TOKEN=$(cat /run/secrets/project_registration_token)
@@ -25,6 +29,7 @@ if [ -n "${DOCKER_VOLUMES_LIST}" ]; then
     IFS=',' read -ra volumes <<< "${DOCKER_VOLUMES_LIST}"
     for volume in "${volumes[@]}"
     do
+        echo "$0: Adding Docker Volume \"${volume}\"..."
         REGISTER_COMMAND="${REGISTER_COMMAND} --docker-volumes \"${volume}\""
     done
 fi
@@ -32,12 +37,15 @@ fi
 # Allow for custom CA file
 if [ -n "${CUSTOM_CA_PATH}" ]; then
     if [[ ! -r "${CUSTOM_CA_PATH}" ]]; then
-        echo "WARN: Custom CA Path <${CUSTOM_CA_PATH}> is not readable. Registration will likely fail"
+        echo "$0: WARN: Custom CA Path <${CUSTOM_CA_PATH}> is not readable. Registration will likely fail"
     else
+        echo "$0: Adding Custom CA \"${CUSTOM_CA_PATH}\"..."
         REGISTER_COMMAND="${REGISTER_COMMAND} --tls-ca-file \"${CUSTOM_CA_PATH}\""
     fi
 fi
 
+echo "$0: Registering this runner..."
+echo "$REGISTER_COMMAND"
 # shellcheck disable=SC2086
 eval $REGISTER_COMMAND
 
